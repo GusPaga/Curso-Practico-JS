bfc45ae15b9adecde99561f7e9ffafa1d75edb56
Gustavo Pagano <gustavopagano03@gmail.com>
2021-12-09 11:23:16 -0300

    escribí el código de las medidas del cuadrado

---
+++ figuras.js
@@ -0,0 +1,10 @@
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm")
+
+const perimetroCadrado = ladoCuadrado * 4;
+console.log("el perímetro del cuadrado mide: " + perimetroCadrado + "cm")
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el área del cuadrado mide: " + areaCuadrado + "cm^2")
+console.groupEnd;
